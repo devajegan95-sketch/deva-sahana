10bee4ddf7ec3071a98504b2663eaf0ea7ce36a7
devajegan95-sketch <devajegan95@gmail.com>
2025-09-17 09:08:11 +0530

    Add files via upload

---
+++ q2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,product;
+	printf("enter afirst num",a);
+	scanf("%d",&a);
+	printf("enter a second num",b);
+	scanf("%d",&b);
+	product=a*b;
+	printf("product between two %d and %d is d", a,b, product);
+	return 0;
+}
