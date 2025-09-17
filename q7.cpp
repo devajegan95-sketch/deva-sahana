0a9cbce2800e07eee39d959fc7b0e83e4877785b
devajegan95-sketch <devajegan95@gmail.com>
2025-09-17 09:12:37 +0530

    Add files via upload

--- q7.cpp
+++ q7.cpp
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(){
-	int a,b;
-	scanf("%d%d",&a,&b);
-	a=b;
-	b=a;
-	printf("%d%d",a,b);
-	return 0;
+	int last digit;       
+	printf("enter num");
+	scanf("%d,%d",a,b);
+	printf(" before swape a=%d,b=%d",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("after swape a=%d,b=%d",a,b);
+  	return 0;
 }
