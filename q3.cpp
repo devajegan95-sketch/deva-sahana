dca5bc2c23762a1e085212e52a5231f67a03ca8f
devajegan95-sketch <devajegan95@gmail.com>
2025-09-17 09:08:37 +0530

    Add files via upload

---
+++ q3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,coefficient;       
+	printf("enter a num",a);
+	scanf("%d",&a);
+	printf("enter a num",b);
+	scanf("%d",&b);
+	coefficient=a*b;
+	printf(" coefficient (product) of %d and %d is %d", a,b, coefficient);
+	return 0;
+}
