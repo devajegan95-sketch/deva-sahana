0b4296965a619790b6c1b331f16eafb0086c5120
devajegan95-sketch <devajegan95@gmail.com>
2025-09-17 09:07:38 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a,b,diff;
+	printf("enter afirst num",a);
+	scanf("%d",&a);
+	printf("enter a second num",b);
+	scanf("%d",&b);
+	diff=a-b;
+	printf("difference between two %d and %d is d", a,b, diff);
+	return 0;
+}
