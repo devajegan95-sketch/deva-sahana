0173d7ca587acb429765a234e70f7d7b7b48c142
devajegan95-sketch <devajegan95@gmail.com>
2025-09-18 14:44:38 +0530

    Delete cq8.cpp

--- cq8.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int n;
-	printf("enter a num:");
-	 scanf("%d ,&n");
-	( n %10)% 3 ==0? printf("last digit divisible by 3\n")
-	                : printf("last digit not divisible by3\n");
-	return 0;
-}
