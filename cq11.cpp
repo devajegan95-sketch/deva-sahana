2a0dea1d0d08581ca1180ba20f9941c04979c67b
devajegan95-sketch <devajegan95@gmail.com>
2025-09-18 14:36:20 +0530

    Add files via upload

--- cq11.cpp
+++ cq11.cpp
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main(){
-	int n;
-	printf("enter a three digit num:");
-	 scanf("%d ,&n");
-	 int first =n /100;
-	(first % 2 ==0)? printf(" first digit is even\n")
-	                : printf("first digit is odd\n");
+	int a,b;
+	printf("enter two num:");
+	scanf("%d %d" , &a, &b);
+	((a+b) % 2== 0) ? printf("sum is even\n"):
+		printf("sum is odd\n");
 	return 0;
-}
+} 
